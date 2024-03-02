86114362d983b08a2bdfb8ae9cb49f168517a174
sarroyo <sarroyo@asrcfederal.com>
2024-03-02 10:35:33 -0500

    Added more to README and DocStringed All The functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 RedisAPIExample
 
-Change yaml template to match your requirements and rename to config.yaml (Keep at top level)
+1) Change yaml template to match your specifications and rename to config.yaml (Keep at top level)
+
+2) When Running this Program, cd into the RedisInterface folder and run main.py from there. 
--- RedisInterface/main.py
+++ RedisInterface/main.py
@@ -4,8 +4,18 @@ import redis
 import json
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
-'''API CALL'''
+
+
 def createResponse(cityName:str) -> str:
+    """    
+    Sends API Request to OpenWeatherAPI
+
+    Args:
+        cityName: Name of the City we want to query. 
+
+    Returns:
+        A JSON String of all the Weather Data at the moment from OpenWeatherAPI
+    """
     url = 'http://api.openweathermap.org/data/2.5/forecast?' + 'q=' + cityName + '&appid=' + str(apiKey)
 
     data = None
@@ -24,6 +34,15 @@ def createResponse(cityName:str) -> str:
 '''REDIS Registering Specific Stuff'''
 
 def setRedisData(cityNames : list[str]) -> None:
+    """    
+    Sends the city data from the OpenWeatherAPI over to Redis via RedisJSON
+
+    Args:
+        cityNames: List of Cities that we want to store in Redis
+
+    Returns:
+        None
+    """
     
     for city in cityNames:
         redisClient.delete(f'WeatherData:{city}') #We only want one instance for this example
@@ -33,27 +52,72 @@ def setRedisData(cityNames : list[str]) -> None:
 
         
 
-def getCityData(city:str) -> dict:  
-    weatherInfo = json.loads(redisClient.json().get(f'WeatherData:{city}'))
+def getCityData(cityName:str) -> dict:  
+    """    
+    Searches in REDIS for a city that we then traverse to get information about the city itself. 
+
+    Args:
+        cityName: Name of the City we want to query. 
+
+    Returns:
+        A Dictionary Containing the City Data in REDIS
+    """
+    weatherInfo = json.loads(redisClient.json().get(f'WeatherData:{cityName}'))
     
     return weatherInfo.get('city')
 
-def getTempatures(city:str) -> tuple[int,int]:
-    weatherInfo = json.loads(redisClient.json().get(f'WeatherData:{city}'))
+def getTempatures(cityName:str) -> tuple[int,int]:
+    """    
+    Searches in REDIS for a city that we then traverse to get information about the tempatures.
+
+    Args:
+        cityName: Name of the City we want to query. 
+
+    Returns:
+        A Tuple Containing the Low and High tempatures for the city. 
+    """
+    weatherInfo = json.loads(redisClient.json().get(f'WeatherData:{cityName}'))
     tempMin = weatherInfo.get('list')[0].get('main').get('temp_min')
     tempMax = weatherInfo.get('list')[0].get('main').get('temp_max')
     print('Temps: Min: ', KelvinToDegrees(tempMin),', Max: ', KelvinToDegrees(tempMax))
     return [KelvinToDegrees(tempMin),KelvinToDegrees(tempMax)]
 
 def KelvinToDegrees(kelvin : int) -> int:
+    """    
+    Converts from Kelvin to Degrees
+
+    Args:
+        kelvin: the tempature to be converted to degrees
+
+    Returns:
+        The conversion of kelvin tempature in Degrees
+    """
     return int((kelvin - 273.15) * (9/5) + 32)
 
-def getElevation(city:str) -> int:
-    weatherInfo = json.loads(redisClient.json().get(f'WeatherData:{city}'))
+def getElevation(cityName:str) -> int:
+    """    
+    Searches in REDIS for a city that we then traverse to get information about the sealevel.
+
+    Args:
+        cityName: Name of the City we want to query. 
+
+    Returns:
+        An int indicating how high above sealevel the respective city is 
+    """
+    weatherInfo = json.loads(redisClient.json().get(f'WeatherData:{cityName}'))
     print(weatherInfo.get('list')[2].get('main').get('sea_level'))
     return int(weatherInfo.get('list')[2].get('main').get('sea_level'))
 
 def plotTempatures(cityNames : list[str]) -> None:
+    """    
+    Plots a comparision of 2 Cities Tempatures
+
+    Args:
+        cityNames:  List of Cities we want to compare (Can only be 2)
+
+    Returns:
+        None
+    """
     if len(cityNames) > 2:
         print("Only supports two cities for now...")
     else:
@@ -81,6 +145,15 @@ def plotTempatures(cityNames : list[str]) -> None:
         plt.show()
 
 def plotElevations(cityNames : list[str]) -> None:
+    """    
+    Plots a comparision of 2 Cities Elevations
+
+    Args:
+        cityNames:  List of Cities we want to compare (Can only be 2)
+
+    Returns:
+        None
+    """
     if len(cityNames) > 2:
         print("Only supports two cities for now...")
     else:
@@ -101,6 +174,15 @@ def plotElevations(cityNames : list[str]) -> None:
         plt.show()
 
 def plotGeoMap(cityNames : list[str]) -> None:
+    """    
+    Plots city locations on a map
+
+    Args:
+        cityNames:  List of Cities we want to visualize
+
+    Returns:
+        None
+    """
     #Set Lat and Lon Coordinate List That We Are Going To Display on Our Map
     lat = []
     lon = []
