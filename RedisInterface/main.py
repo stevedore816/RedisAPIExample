7c0ac7f8b177cb009ab66b69b73cece2d8c9b06c
sarroyo <sarroyo@asrcfederal.com>
2024-03-02 09:13:09 -0500

    Updated Comments & README

--- RedisInterface/main.py
+++ RedisInterface/main.py
@@ -2,22 +2,10 @@ import requests
 import yaml
 import redis
 import json
-'''yaml related stuff'''
-
-try:
-    with open('../config.yaml', 'r') as config:
-        data = yaml.safe_load(config)
-
-    print("Host:", data['redis']['host'])
-except:
-    print("Error pulling data from config.yaml Make sure your in the top directory and following the template appropriately")
+import matplotlib.pyplot as plt
 
 '''API CALL'''
-
-
-apiKey = data['OpenWeatherMapAPI']['api_key']
-def createResponse():
-    cityName = "Philadelphia"
+def createResponse(cityName:str):
     url = "http://api.openweathermap.org/data/2.5/forecast?" + "q=" + cityName + "&appid=" + str(apiKey)
 
     data = None
@@ -35,20 +23,106 @@ def createResponse():
 
 '''REDIS Registering Specific Stuff'''
 
-redisClient = redis.Redis(
+def setRedisData(cityNames = ["Chicago","Philadelphia"]) :
+    
+    for city in cityNames:
+        redisClient.delete(f'WeatherData:{city}') #We only want one instance for this example
+        redisClient.json().set(f'WeatherData:{city}', '.', createResponse(city))
+        '''I was debating between sadd with only weather data or set with WeatherData+city
+        I went with the latter because I feel this is a good use case incase I want temporal data in the future its more organized '''
+
+        
+
+def getCityData(city:str):  
+    weatherInfo = json.loads(redisClient.json().get(f'WeatherData:{city}'))
+    
+    print(weatherInfo.get('city'))
+
+def getTempatures(city:str):
+    weatherInfo = json.loads(redisClient.json().get(f'WeatherData:{city}'))
+    tempMin = weatherInfo.get('list')[0].get('main').get('temp_min')
+    tempMax = weatherInfo.get('list')[0].get('main').get('temp_max')
+    print("Temps: Min: ", KelvinToDegrees(tempMin),", Max: ", KelvinToDegrees(tempMax))
+    return [KelvinToDegrees(tempMin),KelvinToDegrees(tempMax)]
+
+def KelvinToDegrees(kelvin : int) -> int:
+    return int((kelvin - 273.15) * (9/5) + 32)
+
+def getElevation(city:str):
+    weatherInfo = json.loads(redisClient.json().get(f'WeatherData:{city}'))
+    print(weatherInfo.get('list')[2].get('main').get('sea_level'))
+    return weatherInfo.get('list')[2].get('main').get('sea_level')
+
+def plotTempatures(cityNames = ["Chicago","Philadelphia"]) -> None:
+    xMarks = [1,3]
+    xNames = ["Lows","Highs"]
+    plt.xticks(xMarks,xNames)
+    tempatureData = []
+    for city in cityNames:
+        tempatureData.append(getTempatures(city))
+    bar1 = plt.bar(.5,tempatureData[0][0], align='center', alpha=0.5,color='skyblue')
+    plt.bar_label(bar1)
+
+    bar2 = plt.bar(1.3,tempatureData[1][0], align='center', alpha=0.5,color='salmon')
+    plt.bar_label(bar2)
+
+    bar1 = plt.bar(2.5,tempatureData[0][1], align='center', alpha=0.5,color='skyblue')
+    plt.bar_label(bar1)
+
+    bar2 = plt.bar(3.3,tempatureData[1][1], align='center', alpha=0.5,color='salmon')
+    plt.bar_label(bar2)
+
+    plt.xlim([0,4])
+    plt.title("Tempatures for 2/27/2024")
+    plt.legend(["Chicago","Philadelphia"],loc='center right')
+    plt.show()
+
+def plotElevations(cityNames = ["Chicago","Philadelphia"]) -> None:
+    xMarks = [.5,2]
+    xNames = ["Chicago","Philadelphia"]
+    plt.xticks(xMarks,xNames)
+    tempatureData = []
+    for city in cityNames:
+        tempatureData.append(getElevation(city))
+    bar1 = plt.bar(.5,tempatureData[0], align='center', alpha=0.5,color='skyblue')
+    plt.bar_label(bar1)
+
+    bar2 = plt.bar(2,tempatureData[1], align='center', alpha=0.5,color='skyblue')
+    plt.bar_label(bar2)
+
+    plt.xlim([0,2.5])
+    plt.title("SeaLevel")
+    plt.show()
+
+if __name__ == '__main__':
+    #Pulling config data from yaml
+    try:
+        with open('../config.yaml', 'r') as config:
+            data = yaml.safe_load(config)
+    except:
+        print("Error pulling data from config.yaml Make sure your in the top directory and following the template appropriately")
+
+    apiKey = data['OpenWeatherMapAPI']['api_key']
+    
+    #Instantiating Redis Client
+    redisClient = redis.Redis(
     host = data['redis']['host'], 
     port=data['redis']['port'],
     db=data['redis']['db'],
     username=data['redis']['user'],
-    password=data['redis']['password'],
-    ssl=False
-)
+    password=data['redis']['password']
+    )
+    
+    #Extract JSON from Weather API & Send it to Redis DB
+    setRedisData()
+    #GET Functions for Data From REDIS
+    getCityData("Philadelphia")
+    getCityData("Chicago")
+    getTempatures("Philadelphia")
+    getTempatures("Chicago")
+    getElevation("Philadelphia")
+    getElevation("Chicago")
 
-
-redisClient.delete('jsonData')
-
-redisClient.set('WeatherData', createResponse())
-
-print(redisClient.get('WeatherData'))
-
-
+    #Plot City Comparisons
+    #plotTempatures()
+    #plotElevations()
